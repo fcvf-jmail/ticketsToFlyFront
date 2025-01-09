9de1f6970389cd4e68927a3557b98db6779eb5d7
Jmail El Kazzaz <gamelelkazzaz@gmail.com>
2025-01-09 13:59:48 +0300

    eddited dotenv rquirement

--- ticketsToSkyFront.js
+++ ticketsToSkyFront.js
@@ -1,5 +1,5 @@
 const path = require("path")
-(require("dotenv")).config({path: path.join(__dirname, ".env")})
+require("dotenv").config({path: path.join(__dirname, ".env")})
 const { Telegraf, Scenes, session } = require("telegraf");
 const { createSubscriptionScene } = require("./scenes/createSubscriptionScene");
 const bot = new Telegraf(process.env.botToken);
